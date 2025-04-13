7f5d5e3d4553cbac0f1403dcdee7145fdd64832e
Manoj <padigineti.manojkumar@gmail.com>
2025-04-14 00:21:35 +0530

    Did few modifications to catalogue script and frontend script

--- Shell_scripts/catalogue-ms/script.sh
+++ Shell_scripts/catalogue-ms/script.sh
@@ -3,11 +3,11 @@ cp /root/DevSecOps/Shell_scripts/catalogue-ms/mongo.repo /etc/yum.repos.d/mongo.
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-id roboshop &>> /tnp/log.txt
+id roboshop &>> /tmp/log.txt
 if [ $? -ne 0 ]; then
   useradd roboshop
 fi
-ls /app &>> /tnp/log.txt
+ls /app &>> /tmp/log.txt
 if [ $? -ne 0 ]; then
   mkdir /app
 fi
