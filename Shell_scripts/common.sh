a76d5163283748a6b5dff80c19e1743354bcc2d8
Manoj <padigineti.manojkumar@gmail.com>
2025-04-15 19:06:53 +0530

    Added frontend script

--- Shell_scripts/common.sh
+++ Shell_scripts/common.sh
@@ -1,34 +1,31 @@
 install_dependencies () {
-  for dependencies in $1
+ for dependencies in $@
   do
-  if [ $1 == "nginx" ]; then
-    dnf module disable nginx -y
-    dnf module enable nginx:1.24 -y
-    dnf install nginx -y
-  fi
-  if [ $1 == "nodejs" ]; then
-    dnf module disable nginx -y
-    dnf module enable nginx:1.24 -y
-    dnf install nginx -y
-  fi
+    if [ $dependencies == "nginx" ]; then
+      dnf module disable nginx -y
+      dnf module enable nginx:1.24 -y
+      dnf install nginx -y
+      systemd_start nginx
+    fi
+    if [ $dependencies == "nodejs" ]; then
+      dnf module disable nginx -y
+      dnf module enable nginx:1.24 -y
+      dnf install nginx -y
+      systemd_start nodejs
+    fi
   done
+}
 
+systemd_start () {
+  systemctl enable $1
+  systemctl start $1
+  systemctl restart $1
+}
 
-#}
-#}
-#
-#nodejs_install () {
-#  dnf module disable nodejs -y
-#  dnf module enable nodejs:20 -y
-#  dnf install nodejs -y
-#}
-#nginx_install () {
-#dnf module disable nginx -y
-#dnf module enable nginx:1.24 -y
-#dnf install nginx -y
-#}
-#
-#
-#systemd_setup () {
-#  cp /root/DevSecOps/Shell_scripts/catalogue-ms/$app_name.service /etc/systemd/system/$app_name.service
-#}
+nginx_setup () {
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+cp $PWD/nginx.conf /etc/nginx/nginx.conf
+}
--- Shell_scripts/frontend-ms/frontend.sh
+++ Shell_scripts/frontend-ms/frontend.sh
@@ -1,17 +1,4 @@
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-dnf install nginx -y
-
-systemctl enable nginx
-systemctl start nginx
-
-rm -rf /usr/share/nginx/html/*
-
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-
-cp /etc/nginx/nginx.conf
-
-systemctl restart nginx
+source /root/DevSecOps/Shell_scripts/common.sh
+install_dependencies nginx nodejs
+PWD="/root/DevSecOps/Shell_scripts/frontend-ms"
+nginx_setup
