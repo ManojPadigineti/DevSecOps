298864fdb8d77662e6c54acc9a86978b271c19bf
Manoj <padigineti.manojkumar@gmail.com>
2025-04-15 19:41:50 +0530

    modified frontend script

--- Shell_scripts/common.sh
+++ Shell_scripts/common.sh
@@ -35,6 +35,6 @@ ls /usr/share/nginx/html/* ;
  curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
  cd /usr/share/nginx/html
  unzip /tmp/frontend.zip
- cp $current_dir
+ cd $current_dir
  cp -R nginx.conf /etc/nginx/nginx.conf
 }
