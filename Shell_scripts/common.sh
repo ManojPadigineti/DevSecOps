225eb6db2e77cb8979dc25f25e438476efe5bd42
Manoj <padigineti.manojkumar@gmail.com>
2025-04-15 19:17:58 +0530

    modified frontend script

--- Shell_scripts/common.sh
+++ Shell_scripts/common.sh
@@ -23,9 +23,17 @@ systemd_start () {
 }
 
 nginx_setup () {
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-cp $PWD/nginx.conf /etc/nginx/nginx.conf
+ls /usr/share/nginx/html/*
+ if [ $? -eq 0 ]; then
+     curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+     cd /usr/share/nginx/html
+     unzip /tmp/frontend.zip
+     cp $PWD/nginx.conf /etc/nginx/nginx.conf
+ else
+     rm -rf /usr/share/nginx/html/*
+     curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+     cd /usr/share/nginx/html
+     unzip /tmp/frontend.zip
+     cp $PWD/nginx.conf /etc/nginx/nginx.conf
+fi
 }
--- Shell_scripts/frontend-ms/frontend.sh
+++ Shell_scripts/frontend-ms/frontend.sh
@@ -1,4 +1,4 @@
 source /root/DevSecOps/Shell_scripts/common.sh
-install_dependencies nginx nodejs
+install_dependencies nginx
 PWD="/root/DevSecOps/Shell_scripts/frontend-ms"
 nginx_setup
