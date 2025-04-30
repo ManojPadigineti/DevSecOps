44e04d02ec1283894bf8326dd23bdaae1105aa80
Manoj <padigineti.manojkumar@gmail.com>
2025-04-30 19:01:32 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/terraform_modules/modules/ansible_provisioner/main.tf
+++ terraform/AWS/terraform_modules/modules/ansible_provisioner/main.tf
@@ -10,7 +10,9 @@ resource "null_resource" "ansible_provisioner" {
       inline = [
         "cd /home/ec2-user/",
         "sudo dnf install git -y",
-        "git clone https://github.com/ManojPadigineti/DevSecOps_Project.git"
+        "git clone https://github.com/ManojPadigineti/DevSecOps_Project.git",
+        "cd /home/ec2-user/DevSecOps_Project/ansible_terraform",
+        "sudo bash /home/ec2-user/DevSecOps_Project/ansible_terraform/install_ansible.sh"
       ]
     }
 }
