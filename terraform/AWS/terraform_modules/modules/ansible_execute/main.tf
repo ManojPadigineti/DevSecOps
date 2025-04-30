20046d50361cb3fc05c685e7cdbf21ab882ddfb9
Manoj <padigineti.manojkumar@gmail.com>
2025-04-30 21:06:37 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/terraform_modules/modules/ansible_execute/main.tf
+++ terraform/AWS/terraform_modules/modules/ansible_execute/main.tf
@@ -1,19 +1,14 @@
 resource "null_resource" "ansible_playbook" {
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = var.password
-    host     = var.server_ip
-  }
 
   provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = var.password
+      host     = var.server_ip
+    }
     inline = [
-      "sudo -i ; cd /home/ec2-user/DevSecOps_Project/ansible_terraform"
-      # "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=${var.instances}"
-    ]
-  }
-  provisioner "remote-exec" {
-    inline = [
+      "sudo -i ; cd /home/ec2-user/DevSecOps_Project/ansible_terraform",
       "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=mongodb",
       "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=redis",
       "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=mysql",
@@ -26,6 +21,5 @@ resource "null_resource" "ansible_playbook" {
       "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=dispatch",
       "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=frontend"
     ]
-
   }
 }
