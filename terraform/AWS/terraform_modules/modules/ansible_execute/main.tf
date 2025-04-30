725616c91df3e5d46393488baf485cd584ee4d92
Manoj <padigineti.manojkumar@gmail.com>
2025-04-30 20:01:53 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/terraform_modules/modules/ansible_execute/main.tf
+++ terraform/AWS/terraform_modules/modules/ansible_execute/main.tf
@@ -8,6 +8,7 @@ resource "null_resource" "ansible_playbook" {
       host     = var.server_ip
     }
     inline = [
+      "sudo -i",
       "cd /home/ec2-user/DevSecOps_Project/ansible_terraform",
       "ansible-playbook -i inv.txt playbook.yml -e var_file=${var.instances}"
     ]
