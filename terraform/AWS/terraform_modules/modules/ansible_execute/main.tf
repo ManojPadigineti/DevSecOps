cf38c0d87041ec27199a3099882b4ef6e2a35cf4
Manoj <padigineti.manojkumar@gmail.com>
2025-04-30 20:43:01 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/terraform_modules/modules/ansible_execute/main.tf
+++ terraform/AWS/terraform_modules/modules/ansible_execute/main.tf
@@ -1,16 +1,20 @@
 resource "null_resource" "ansible_playbook" {
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = var.password
+    host     = var.server_ip
+  }
 
   provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      password = var.password
-      host     = var.server_ip
-    }
     inline = [
-      "sudo -i",
-      "cd /home/ec2-user/DevSecOps_Project/ansible_terraform",
+      "sudo -i ; cd /home/ec2-user/DevSecOps_Project/ansible_terraform"
+    ]
+  }
+  provisioner "remote-exec" {
+    inline = [
       "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=${var.instances}"
     ]
+
   }
 }
