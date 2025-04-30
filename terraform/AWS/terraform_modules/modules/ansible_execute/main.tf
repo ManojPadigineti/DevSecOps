dea4f792f609bac3bd18a4812ae9180bd5ff3fed
Manoj <padigineti.manojkumar@gmail.com>
2025-04-30 21:08:54 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/terraform_modules/modules/ansible_execute/main.tf
+++ terraform/AWS/terraform_modules/modules/ansible_execute/main.tf
@@ -9,17 +9,17 @@ resource "null_resource" "ansible_playbook" {
     }
     inline = [
       "sudo -i ; cd /home/ec2-user/DevSecOps_Project/ansible_terraform",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=mongodb",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=redis",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=mysql",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=rabbitmq",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=catalogue",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=user",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=cart",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=shipping",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=payment",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=dispatch",
-      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv.txt playbook.yml -e var_file=frontend"
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=mongodb",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=redis",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=mysql",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=rabbitmq",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=catalogue",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=user",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=cart",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=shipping",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=payment",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=dispatch",
+      "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -vvv -i inv.txt playbook.yml -e var_file=frontend"
     ]
   }
 }
