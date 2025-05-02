628d5a662e44d46f785af92b4d5c267502dff0d6
Manoj <padigineti.manojkumar@gmail.com>
2025-05-02 23:35:57 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/roboshop_project/modules/nat/main.tf
+++ terraform/AWS/roboshop_project/modules/nat/main.tf
@@ -7,7 +7,7 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = var.private_subnet_nat
+  subnet_id     = var.public_subnet_nat
   tags = {
     Name = var.nat_name
   }
