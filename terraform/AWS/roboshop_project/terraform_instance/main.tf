9874a7e8d1e1cf25a8e1b89ce4ae4218ffc0b8c9
Manoj <padigineti.manojkumar@gmail.com>
2025-05-02 23:05:28 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/roboshop_project/terraform_instance/main.tf
+++ terraform/AWS/roboshop_project/terraform_instance/main.tf
@@ -7,3 +7,10 @@ module "terraform_ec2" {
   instance_type = each.value.instance_type
   security_group = data.aws_security_group.security_group.id
 }
+
+module "eip" {
+  depends_on = [module.terraform_ec2]
+  for_each = var.terraform_instance
+  source = "../modules/eip"
+  instance_id = module.terraform_ec2[each.key].ec2_instance_output_id
+}
