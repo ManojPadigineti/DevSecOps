5bc5ca06d03607604be7b94d7ebb33a822b733fb
Manoj <padigineti.manojkumar@gmail.com>
2025-05-03 15:13:36 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/roboshop_project/modules/ansible_provisioner/main.tf
+++ terraform/AWS/roboshop_project/modules/ansible_provisioner/main.tf
@@ -11,7 +11,7 @@ resource "null_resource" "terraform_provisioner" {
     inline = [
        "git clone https://github.com/ManojPadigineti/ansible_playbook.git",
         "sudo bash ansible_playbook/install_ansible.sh",
-        "cd /home/ec2-user/ansible_playbook/ ; ansible-playbook -i localhost playbook.yml -e var_file=${var.server_name}"
+        "cd /home/ec2-user/ansible_playbook/ ; git pull; ansible-playbook -i localhost playbook.yml -e var_file=${var.server_name}"
     ]
   }
 }
