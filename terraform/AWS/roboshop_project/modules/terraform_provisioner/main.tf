b7e8d2dee06a90f806fee7d1adce2c6e323f1e4f
Manoj <padigineti.manojkumar@gmail.com>
2025-05-03 12:37:38 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/roboshop_project/modules/terraform_provisioner/main.tf
+++ terraform/AWS/roboshop_project/modules/terraform_provisioner/main.tf
@@ -2,7 +2,7 @@ resource "null_resource" "terraform_provisioner" {
 
   connection {
     type     = "ssh"
-    user     = "root"
+    user     = "ec2-user"
     password = var.password
     host     = var.public_ip
   }
