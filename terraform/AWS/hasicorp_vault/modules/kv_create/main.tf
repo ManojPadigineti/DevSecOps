b05079feccdcbd31092a1b020d7c8a6cc3014d84
Manoj <padigineti.manojkumar@gmail.com>
2025-05-07 15:32:59 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/hasicorp_vault/modules/kv_create/main.tf
+++ terraform/AWS/hasicorp_vault/modules/kv_create/main.tf
@@ -1,29 +1,10 @@
 
-resource "vault_mount" "kv2" {
-  path        = "/v1/v1/manoj/data/credentials/data/devops"
-  type        = "kv"
-  options     = { version = "2" }
-}
-
-resource "vault_kv_secret_v2" "example" {
-  mount               = vault_mount.kv2.path
-  name                = "devops"
-  delete_all_versions = false
-  data_json = jsonencode(
-    {
-      name = "manoj",
-      role = "devops",
-      offer = "yes"
-    }
-  )
-}
-
-data "vault_kv_secret_v2" "example" {
-  mount = "manoj"
+data "vault_kv_secret_v2" "credentials" {
+  mount = "/v1/kv/data/credentials"
   name  = "credentials"
 }
 
 resource "local_file" "copy_to_local" {
   filename = "credentials_from_vault"
-  content = data.vault_kv_secret_v2.example.data_json
+  content = data.vault_kv_secret_v2.credentials.data["password"]
 }
