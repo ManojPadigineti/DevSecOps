f9e7e8fb7efde342195da2347a449a2036901e1f
Manoj <padigineti.manojkumar@gmail.com>
2025-05-07 20:03:05 +0530

    Terraform Modules, Project Setup

--- terraform/AWS/roboshop_project/modules/terraform_provisioner/main.tf
+++ terraform/AWS/roboshop_project/modules/terraform_provisioner/main.tf
@@ -9,10 +9,10 @@ resource "null_resource" "terraform_provisioner" {
 
   provisioner "remote-exec" {
     inline = [
-       "git clone https://github.com/ManojPadigineti/ansible_playbook.git",
+        "git clone https://github.com/ManojPadigineti/ansible_playbook.git",
         "git pull ; sudo bash ansible_playbook/Install_terraform.sh",
         "git pull ; sudo bash ansible_playbook/install_ansible.sh",
-         "git pull ; ansible-playbook -i localhost playbook.yml -e var_file=hashicorp"
+        "git pull ; ansible-playbook -i localhost playbook.yml -e var_file=hashicorp"
     ]
   }
 }
